b8ea4635b685db3e9790183fbeb7963f5cbe8ddb
Simon Krampe <smnkrmp@gmail.com>
2023-04-02 16:57:59 +0200

    removed sorting utils

--- imgthings/src/main/java/com/simonkrampe/sorter.java
+++ imgthings/src/main/java/com/simonkrampe/sorter.java
@@ -1,14 +1,11 @@
 package com.simonkrampe;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 public class sorter {
 
-    sortingUtils sU;
-
     public sorter(){
-
-        sU = new sortingUtils();
 
     }
 
@@ -17,7 +14,7 @@ public class sorter {
      */
     public BufferedImage sortHorizontalDescending(BufferedImage masked, BufferedImage image) {
 
-        int[][] mask = sU.convertMaskHorizontal(masked);
+        int[][] mask = convertMaskHorizontal(masked);
 
         int width = masked.getWidth();
         int height = masked.getHeight();
@@ -36,7 +33,7 @@ public class sorter {
                         pixelsToSort[i] = image.getRGB(x+i, y);
                     }
 
-                    int[] sorted = sU.sortDescending(pixelsToSort);
+                    int[] sorted = sortDescending(pixelsToSort);  
 
                     for(int i = 0; i < length; i++){
                         image.setRGB(x+i, y, sorted[i]);
@@ -48,6 +45,72 @@ public class sorter {
         }
 
         return image;
+
+    }
+
+    
+    public int[] sortDescending(int[] pixelsToSort) {
+    
+        boolean sorted = false;
+        while(sorted == false){
+            boolean correctStreak = true;
+            for(int i = 0; i < pixelsToSort.length-1; i++){
+    
+                if(pixelsToSort[i] < pixelsToSort[i + 1]){
+
+                    int Position1 = pixelsToSort[i];
+                    int Position2 = pixelsToSort[i + 1];
+    
+                    pixelsToSort[i] = Position2;
+                    pixelsToSort[i + 1] = Position1;
+    
+                    correctStreak = false;
+                    
+                }
+            }
+            
+            if(correctStreak == true)
+                sorted = true;
+
+        }
+    
+        return pixelsToSort;
+    
+    }
+    
+    
+
+    public int[][] convertMaskHorizontal(BufferedImage image) {
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        int[][] mask = new int[width][height];
+        
+        int white = Color.white.getRGB();
+
+        for(int y = 0; y < height; y++){
+
+            for(int x = 0; x < width; x++){
+
+                int pixel = image.getRGB(x, y);
+                
+                if(pixel == white){
+                    int start = x;
+                    int length = 0;
+                    while(pixel == white && x < width){
+                        pixel = image.getRGB(x, y);
+                        x++;
+                        length++;
+                    }
+                    mask[start][y] = length;
+                }
+
+            }
+
+        }
+
+        return mask;
 
     }
 
