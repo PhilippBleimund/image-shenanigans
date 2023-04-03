f26c4833ee85ae6df9ceb4c26ea155a5ab200c35
Simon Krampe <smnkrmp@gmail.com>
2023-04-03 12:07:14 +0200

    Revert "Added testing.java to .gitignore"
    This reverts commit 3c810c6e2062db42cb6d9a05dda8f628004a6226.

---
+++ imgthings/src/main/java/com/simonkrampe/testing.java
@@ -0,0 +1,46 @@
+package com.simonkrampe;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import javax.imageio.ImageIO;
+
+public class testing {
+    
+    public static void main (String[] args) {
+
+        handler h = new handler();
+        mask m = new mask();
+        sorter s = new sorter();
+
+        File f = new File(System.getProperty("user.dir") + "/imgthings/src/main/java/com/simonkrampe/test2.jpg");
+
+        BufferedImage image = h.bufferImage(f.toString());
+        BufferedImage masked = m.byColor(image, -13000000);
+
+        
+        try {
+            File outputfile = new File("mask.jpg");
+            ImageIO.write(masked, "jpg", outputfile);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        BufferedImage ogImage = h.bufferImage(f.toString());
+        BufferedImage sorted = s.sortHorizontalDescending(masked, ogImage);
+
+        try {
+            File savefile = new File("sorted.jpg");
+            ImageIO.write(sorted, "jpg", savefile);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
