dfc097de1c24c75c098cc3d317b77c31ea7b8209
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-05 21:33:44 +0200

    remove SwingWorker from imageEffect

--- imgthings/src/main/java/com/simonkrampe/imageEffects/imageEffect.java
+++ imgthings/src/main/java/com/simonkrampe/imageEffects/imageEffect.java
@@ -3,44 +3,24 @@ package com.simonkrampe.imageEffects;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.swing.SwingWorker;
-
-import com.simonkrampe.listener.ProgressEvent;
-import com.simonkrampe.listener.ProgressListener;
 import com.simonkrampe.wrapper.requestData;
 
-public abstract class imageEffect extends SwingWorker{
+public abstract class imageEffect{
     
-    public enum status{
-        DONE
-    };
-
     public BufferedImage originalImage;
 
     public BufferedImage newImage;
 
     public requestData data;
 
-    private List<ProgressListener> Listeners = new ArrayList<ProgressListener>();
+	public abstract void imageTransformation();
 
 	protected imageEffect(requestData data, renderJob motherJob){
 		this.data = data;
 
 		this.originalImage = motherJob.getWorkingImage();
 		this.newImage = deepCopy(this.originalImage);
-	}
-
-    public void addListener(ProgressListener listener) {
-		Listeners.add(listener);
-	}
-	
-	protected void notifyListener(status s) {
-		for(ProgressListener L : Listeners) {
-			L.changeProgressStatus(new ProgressEvent(s, this));
-		}
 	}
 
     protected BufferedImage deepCopy(BufferedImage bi) {
@@ -50,11 +30,11 @@ public abstract class imageEffect extends SwingWorker{
 		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
 	}
 
+	public void setImage(BufferedImage image){
+		this.originalImage = image;
+	}
+
     public BufferedImage getFinishedRender() {
-		if(this.isDone()) {
-			return this.newImage;
-		}else {
-			return null;
-		}
+		return this.newImage;
 	}
 }
