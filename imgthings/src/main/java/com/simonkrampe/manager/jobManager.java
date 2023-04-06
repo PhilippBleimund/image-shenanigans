95ccf27065207abf56ba179de1573c761aea5ba2
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-06 22:20:39 +0200

    Add Lists

--- imgthings/src/main/java/com/simonkrampe/manager/jobManager.java
+++ imgthings/src/main/java/com/simonkrampe/manager/jobManager.java
@@ -1,12 +1,20 @@
 package com.simonkrampe.manager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.simonkrampe.imageEffects.renderJob;
+
 public class jobManager {
     
     public jobManager INSTANCE;
 
+    public List<renderJob> preloadJobs = new ArrayList<renderJob>();
+
+    public List<renderJob> QueuedJobs = new ArrayList<renderJob>();
 
 
-    
+
     public jobManager getInstance(){
         if(this.INSTANCE == null)
             this.INSTANCE = new jobManager();
