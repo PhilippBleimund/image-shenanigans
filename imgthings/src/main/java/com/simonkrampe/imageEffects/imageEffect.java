1027640190d83433b00f6924fe3795cdca01acf6
Philipp Bleimund <maginir.philipp@gmail.com>
2023-04-07 17:46:09 +0200

    add renderTime to imageEffect

--- imgthings/src/main/java/com/simonkrampe/imageEffects/imageEffect.java
+++ imgthings/src/main/java/com/simonkrampe/imageEffects/imageEffect.java
@@ -14,6 +14,8 @@ public abstract class imageEffect{
 
     public requestData data;
 
+	public long renderTimeTaken;
+
 	public abstract void imageTransformation();
 
 	protected imageEffect(requestData data, renderJob motherJob){
@@ -36,5 +38,41 @@ public abstract class imageEffect{
 
     public BufferedImage getFinishedRender() {
 		return this.newImage;
+	}
+
+	public String getRenderTime(){
+		StringBuilder buf = new StringBuilder(20);
+		String sgn = "";
+
+		if (renderTimeTaken < 0) {
+			sgn = "-";
+			renderTimeTaken = Math.abs(renderTimeTaken);
+		}
+
+		append(buf, sgn, 0, (renderTimeTaken / 3600000));
+		renderTimeTaken %= 3600000;
+		append(buf, ":", 2, (renderTimeTaken / 60000));
+		renderTimeTaken %= 60000;
+		append(buf, ":", 2, (renderTimeTaken / 1000));
+		renderTimeTaken %= 1000;
+		append(buf, ".", 3, (renderTimeTaken));
+		return buf.toString();
+	}
+
+	/**
+	 * Append a right-aligned and zero-padded numeric value to a `StringBuilder`.
+	 */
+	static private void append(StringBuilder tgt, String pfx, int dgt, long val) {
+		tgt.append(pfx);
+		if (dgt > 1) {
+			int pad = (dgt - 1);
+			for (long xa = val; xa > 9 && pad > 0; xa /= 10) {
+				pad--;
+			}
+			for (int xa = 0; xa < pad; xa++) {
+				tgt.append('0');
+			}
+		}
+		tgt.append(val);
 	}
 }
